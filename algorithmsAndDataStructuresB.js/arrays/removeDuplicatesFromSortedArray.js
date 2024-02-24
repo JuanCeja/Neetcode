aa6ee64bc0d66ac3aa4889761a73fed32e410b05
JuanCeja <jmc347ls2@yahoo.com>
2024-02-23 17:14:41 -0800

    create pseudo code for removeDuplicatesFromSortedArray

--- algorithmsAndDataStructuresB.js/arrays/removeDuplicatesFromSortedArray.js
+++ algorithmsAndDataStructuresB.js/arrays/removeDuplicatesFromSortedArray.js
@@ -24,7 +24,8 @@
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
 const removeDuplicates = (nums) => {
-
+    
 };
 
-console.log(removeDuplicates())
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])); // [0,1,2,3,4,_,_,_,_,_], 5
+console.log(removeDuplicates([1,1,2])); // [1,2,_], 2
