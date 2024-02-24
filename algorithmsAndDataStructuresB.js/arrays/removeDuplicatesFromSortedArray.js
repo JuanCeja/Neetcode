9eb585bc1bf3a6995885214ec54101546f0282c0
JuanCeja <jmc347ls2@yahoo.com>
2024-02-23 17:26:31 -0800

    create a solution for removeDuplicatesFromSortedArray

--- algorithmsAndDataStructuresB.js/arrays/removeDuplicatesFromSortedArray.js
+++ algorithmsAndDataStructuresB.js/arrays/removeDuplicatesFromSortedArray.js
@@ -24,8 +24,20 @@
 // It does not matter what you leave beyond the returned k (hence they are underscores).
 
 const removeDuplicates = (nums) => {
-    
+    let map = {};
+    map[nums[0]] = true;
+    let pointer = 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (!map[nums[i]]) {
+            map[nums[i]] = true;
+            [nums[i], nums[pointer]] = [nums[pointer], nums[i]];
+            pointer++;
+        } else {
+            nums[i] = "_"
+        }
+    }
+    return nums;
 };
 
 console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4])); // [0,1,2,3,4,_,_,_,_,_], 5
-console.log(removeDuplicates([1,1,2])); // [1,2,_], 2
+console.log(removeDuplicates([1, 1, 2])); // [1,2,_], 2
