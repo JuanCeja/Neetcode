66e1b478ed58be74c1c203f65b7e0a2b062b8e9b
JuanCeja <jmc347ls2@yahoo.com>
2024-02-27 19:52:58 -0800

    create solution for reverse linked list

--- algorithmsAndDataStructuresB.js/linkedList/reveresLinkedList.js
+++ algorithmsAndDataStructuresB.js/linkedList/reveresLinkedList.js
@@ -13,18 +13,19 @@ class LinkedList {
 
     reverse() {
         let current = this.head;
-        this.head = this.tail;
-        this.tail = current;
-        let next = current.next;
-        let pointer = next.next;
-        // iterate the linkedList
-        while (pointer.next !== null) {
-            // as we iterate we point the next to our current
-            next.next = current;
-            current = next;
-            next = current.next;
-            pointer = next.next;
+        let prev = null;
+        let nxt;
+        let newTail = this.head;
+
+        while (current) {
+            nxt = current.next;
+            current.next = prev;
+            prev = current;
+            current = nxt;
         }
-        // return our linked list
+
+        this.head = prev;
+        this.tail = newTail;
+        return this;
     }
 }
