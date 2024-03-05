dc27e4934a05acacb875e2603623b35edd304b74
JuanCeja <jmc347ls2@yahoo.com>
2024-03-04 18:20:49 -0800

    initial commit for merge two sorted linked lists

--- algorithmsAndDataStructuresB.js/linkedList/reveresLinkedList.js
+++ algorithmsAndDataStructuresB.js/linkedList/reveresLinkedList.js
@@ -9,7 +9,7 @@ class LinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
-    }
+    };
 
     reverse() {
         let current = this.head;
@@ -24,14 +24,9 @@ class LinkedList {
         }
         this.head = prev;
         return this;
-    }
+    };
 
-    reverseRecursive() {
-        // Special case...
-        // Create a new node to call the function recursively and we get the reverse linked list...
-        // Set head node as head.next.next...
-        //set head's next to be null...
-        // Return the reverse linked list...
-
-    }
+    mergeTwoSortedLists(list1, list2) {
+        
+    };
 }
