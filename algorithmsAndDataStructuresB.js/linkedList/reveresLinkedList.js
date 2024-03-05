96127307e6548cd9b587a9ed2933a7702427f594
JuanCeja <jmc347ls2@yahoo.com>
2024-03-04 18:57:12 -0800

    create pseudo code for merge two sorted linked list

--- algorithmsAndDataStructuresB.js/linkedList/reveresLinkedList.js
+++ algorithmsAndDataStructuresB.js/linkedList/reveresLinkedList.js
@@ -27,6 +27,30 @@ class LinkedList {
     };
 
     mergeTwoSortedLists(list1, list2) {
-        
+        // current and pointers
+        let current;
+        let p1 = list1.head;
+        let p2 = list2.head;
+        // compare both heads
+        if(p1 === p2) {
+            current = p1;
+            p1 = p1.next;
+        } else {
+            current = Math.min(p1, p2);
+        }
+        // while both pointers
+        while(p1 && p2) {
+            // if p1 < p2
+            if(p1 < p2) {
+
+            }
+        }
+                // current.next = p1
+                // move p1 up
+            // else if p2 < p1
+                // current.next = p2;
+                // move p2 up
+        // return list;
+            
     };
 }
