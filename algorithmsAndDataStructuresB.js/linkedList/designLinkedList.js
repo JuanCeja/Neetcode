db9fab0a890035b9fde7072570dd643b4c75e9c4
JuanCeja <jmc347ls2@yahoo.com>
2024-03-05 20:46:02 -0800

    create Node and MyLinkedList classes

--- algorithmsAndDataStructuresB.js/linkedList/designLinkedList.js
+++ algorithmsAndDataStructuresB.js/linkedList/designLinkedList.js
@@ -16,6 +16,17 @@
 
 // void deleteAtIndex(int index) Delete the indexth node in the linked list, if the index is valid.
 
-class LinkedList {
-    
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class MyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
 }
