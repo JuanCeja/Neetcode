1b58d41c31759050193884067c692409866c2c28
JuanCeja <jmc347ls2@yahoo.com>
2024-03-05 21:17:31 -0800

    make adjustments to code to optimize solutions

--- algorithmsAndDataStructuresB.js/linkedList/designLinkedList.js
+++ algorithmsAndDataStructuresB.js/linkedList/designLinkedList.js
@@ -31,7 +31,7 @@ class MyLinkedList {
     }
 
     get(index) {
-        if (index > this.length) return -1;
+        if (index >= this.length) return -1;
 
         let counter = 0;
         let current = this.head;
@@ -44,35 +44,49 @@ class MyLinkedList {
 
     addAtHead(val) {
         let newNode = new Node(val);
-        newNode.next = this.head;
-        this.head = newNode;
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
         return this;
     }
 
     addAtTail(val) {
         let newNode = new Node(val);
-        this.tail.next = newNode;
-        this.tail = newNode;
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        };
+        this.length++;
+        return this;
     }
 
     addAtIndex(index, val) {
-        if (index === this.length) this.addAtTail(val);
-        else if (index > this.length) return;
+        if (index === 0) return this.addAtHead(val);
+        if (index === this.length) return this.addAtTail(val);
+        if (index > this.length) return;
 
         let newNode = new Node(val);
-
-        let target = index - 1;
-        let counter = 0;
+        let prevNode = null;
         let current = this.head;
-        while (current) {
+        let counter = 0;
+
+        while (counter < index) {
+            prevNode = current;
             current = current.next;
             counter++;
-            if (counter === target) {
-                newNode.next = current.next.next;
-                current.next = newNode;
-                return this;
-            }
         }
+        
+        newNode.next = current;
+        prevNode.next = newNode;
+        this.length++;
         return this;
     }
 }
