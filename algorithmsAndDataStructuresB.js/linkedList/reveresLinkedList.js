d0380aa977809767eca387f79007f153b02979e8
JuanCeja <jmc347ls2@yahoo.com>
2024-03-09 12:21:01 -0800

    complete solution for reverse a singly linked list

--- algorithmsAndDataStructuresB.js/linkedList/reveresLinkedList.js
+++ algorithmsAndDataStructuresB.js/linkedList/reveresLinkedList.js
@@ -12,7 +12,22 @@ class LinkedList {
     };
 
     reverse() {
-        
+        if(!this.head) return null;
+
+        let current = this.head;
+        let prev = null;
+        let next = current ? current.next : null;
+        this.tail = current;
+
+        while(current) {
+            current.next = prev;
+            prev = current;
+            current = next;
+            next = current.next;
+        }
+
+        this.head = prev;
+        return this;
     };
 
     mergeTwoSortedLists(list1, list2) {
