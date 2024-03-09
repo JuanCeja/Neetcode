85eb0fa698b295a0197c191cc1688a74c5c7c284
JuanCeja <jmc347ls2@yahoo.com>
2024-03-09 12:16:04 -0800

    initial commit for reverse a linked list

--- algorithmsAndDataStructuresB.js/linkedList/reveresLinkedList.js
+++ algorithmsAndDataStructuresB.js/linkedList/reveresLinkedList.js
@@ -12,18 +12,7 @@ class LinkedList {
     };
 
     reverse() {
-        let current = this.head;
-        let prev = null;
-        let next = current ? current.next : null;
-        this.tail = current
-        while (current) {
-            current.next = prev;
-            prev = current;
-            current = next;
-            next = current.next;
-        }
-        this.head = prev;
-        return this;
+        
     };
 
     mergeTwoSortedLists(list1, list2) {
