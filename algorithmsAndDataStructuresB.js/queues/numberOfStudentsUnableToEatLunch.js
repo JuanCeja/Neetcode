fff02137f5ac18ff9b2a5361e7e10ad29b0408a4
JuanCeja <jmc347ls2@yahoo.com>
2024-03-08 21:36:45 -0800

    re initial commit

--- algorithmsAndDataStructuresB.js/queues/numberOfStudentsUnableToEatLunch.js
+++ algorithmsAndDataStructuresB.js/queues/numberOfStudentsUnableToEatLunch.js
@@ -44,69 +44,23 @@ class Queue {
     }
 
     enqueue(data) {
-        let newNode = new QueueNode(data);
-
-        if (this.tail === null) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.size++;
     }
 
     dequeue() {
-        if (this.isEmpty()) return null;
-
-        let removedNode = this.head;
-        this.head = removedNode.next;
-
-        if (this.head === null) this.tail = null;
-
-        this.size--;
-        return removedNode.data;
     }
 
     isEmpty() {
-        return this.size === 0;
     }
 
     peek() {
-        if (this.isEmpty()) return null
-        return this.head.data;
     }
 
     getSize() {
-        return this.size;
     }
 }
 
 const countStudents = (students, sandwiches) => {
-    const len = students.length;
-    const studentsQueue = new Queue();
-    const foodStack = new Array();
-
-    for (let i = 0; i < len; i++) {
-        studentsQueue.enqueue(students[i]);
-        foodStack.push(sandwiches[len - 1 - i]);
-    }
-
-    let counter = 0;
-
-    while (counter < foodStack.length) {
-        if (studentsQueue.peek() === foodStack[foodStack.length - 1]) {
-            studentsQueue.dequeue();
-            foodStack.pop();
-            counter = 0;
-        } else {
-            const deqVal = studentsQueue.dequeue();
-            studentsQueue.enqueue(deqVal);
-            counter++;
-        }
-    }
-
-    return counter;
+    
 };
 
 console.log(countStudents([1, 1, 0, 0], [0, 1, 0, 1])); // 0
