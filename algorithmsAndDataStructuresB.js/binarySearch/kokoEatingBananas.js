866e74f4c8e1ec2f89169b24b3ab896eb1fd62a5
JuanCeja <jmc347ls2@yahoo.com>
2024-03-21 16:47:08 -0700

    re initial commit for kokoEatingBananas algorithm

--- algorithmsAndDataStructuresB.js/binarySearch/kokoEatingBananas.js
+++ algorithmsAndDataStructuresB.js/binarySearch/kokoEatingBananas.js
@@ -21,24 +21,7 @@
 // Output: 23
 
 const minEatingSpeed = (piles, h) => {
-    let min = 1;
-    let max = Math.max(...piles);
-    let best = max;
-
-    const time = (speed) => piles.reduce((sum, pile) => sum + Math.ceil(pile / speed), 0);
-
-    while (min <= max) {
-        let mid = Math.floor((min + max) / 2);
-
-        if (time(mid) <= h) {
-            best = mid;
-            max = mid - 1;
-        } else {
-            min = mid + 1;
-        }
-    };
-
-    return best;
+    
 };
 
 console.log(minEatingSpeed([3, 6, 7, 11], 8)); // 4
