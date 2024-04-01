18a8373ea78a52c241ce7acb76d2382038668853
JuanCeja <jmc347ls2@yahoo.com>
2024-04-01 14:45:37 -0700

    create a iterative solution using a stack for kthSmallestElement method for a binarySearchTree

--- algorithmsAndDataStructuresB.js/trees/binarySearchTree.js
+++ algorithmsAndDataStructuresB.js/trees/binarySearchTree.js
@@ -116,7 +116,25 @@ class BinarySearchTree {
     }
 
     kthSmallestElement(root, k) {
-        
+        let n = 0;
+        let stack = [];
+        let current = root;
+
+        while(current || stack.length > 0) {
+
+            while(current) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            n++;
+            if (n === k) return current.val;
+
+            current = current.right;
+        }
+
+        return -1;
     }
 }
 
