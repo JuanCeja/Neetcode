bb0c7131e10f0dc9423bd2011dbedccd5173b432
JuanCeja <jmc347ls2@yahoo.com>
2024-04-01 21:58:41 -0700

    create solution for constructBSTFromPreorderAndInorder algorithm

--- algorithmsAndDataStructuresB.js/trees/binarySearchTree.js
+++ algorithmsAndDataStructuresB.js/trees/binarySearchTree.js
@@ -138,7 +138,20 @@ class BinarySearchTree {
     }
 
     constructBSTFromPoAndIo(preOrder, inOrder) {
-        
+        let p = 0;
+        let i = 0;
+
+        const build = (stop) => {
+            if (inOrder[i] != stop) {
+                let root = new TreeNode(preOrder[p++]);
+                root.left = build(root.val);
+                i++;
+                root.right = build(stop);
+                return root;
+            }
+            return null;
+        }
+        return build();
     }
 }
 
