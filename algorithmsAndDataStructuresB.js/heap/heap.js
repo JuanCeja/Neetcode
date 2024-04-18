0e5cd07e9378711c7d7f3ccc8941ffd1cddf977b
Juan Ceja MBP <jmc347ls2@yahoo.com>
2024-04-18 10:14:23 -0700

    attemp solution for push method for our Heap class

--- algorithmsAndDataStructuresB.js/heap/heap.js
+++ algorithmsAndDataStructuresB.js/heap/heap.js
@@ -1,6 +1,21 @@
 class Heap {
-    constructor() {
-        this.heap = new Array();
-        this.heap.push(0);
+  constructor() {
+    this.heap = new Array();
+    this.heap.push(0);
+  }
+
+  push(val) {
+    if (this.heap.length <= 1) return this.heap.push(val);
+
+    this.heap.push(val);
+
+    let current = this.heap.length - 1;
+    let parent = Math.floor(current / 2);
+
+    while (this.heap[current] > this.heap[parent]) {
+        [this.heap[current], this.heap[parent]] = [this.heap[parent], this.heap[current]];
     }
+
+    return this;
+  }
 }
