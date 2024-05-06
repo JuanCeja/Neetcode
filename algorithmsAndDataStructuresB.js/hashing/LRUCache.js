821a40914e0f5a97d6b2f85b74d1a8a41c8c5b70
Juan Ceja MBP <jmc347ls2@yahoo.com>
2024-05-06 10:19:28 -0700

    complete get method for our LRUCache

--- algorithmsAndDataStructuresB.js/hashing/LRUCache.js
+++ algorithmsAndDataStructuresB.js/hashing/LRUCache.js
@@ -30,5 +30,19 @@
 
 
 class LRUCache {
+    constructor(capacity) {
+        this.cache = new Map();
+        this.capacity = capacity;
+    }
+
+    get(key) {
+        if(!this.cache.has(key)) return -1;
+
+        const v = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, v);
+        return this.cache.get(key);
+    }
+
     
 }
