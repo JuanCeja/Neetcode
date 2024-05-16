86212d92bc19f75923da6ce39cf604d013214744
Juan Ceja MBP <jmc347ls2@yahoo.com>
2024-05-16 10:12:15 -0700

    attemp solution for DFS using a matrix

--- algorithmsAndDataStructuresB.js/graphs/matrix-DFS/DFSMatrix.js
+++ algorithmsAndDataStructuresB.js/graphs/matrix-DFS/DFSMatrix.js
@@ -1,3 +1,24 @@
 const dfs = (grid, r, c, visit) => {
-    
+  let ROWS = grid.length,
+    COLS = grid[0].length;
+
+  if (
+    Math.min(r, c) < 0 ||
+    r >= ROWS ||
+    c >= COLS ||
+    grid[r][c] === 1 ||
+    visit.has(r, c)
+  )
+    return 0;
+
+  if (r === ROWS - 1 && c === COLS - 1) return 1;
 };
+
+const grid = [
+  [0, 0, 0, 0],
+  [1, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 1, 0, 0],
+];
+
+console.log(dfs(grid, 0, 0, set()));
